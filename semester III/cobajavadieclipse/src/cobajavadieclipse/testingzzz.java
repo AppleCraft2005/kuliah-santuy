83dd4ec3c73c94dfd7d2d6421d808bdd8d719d41
AppleCraft <jovangilbert737@gmail.com>
2024-09-07 12:59:18 +0800

    menambahkan java

---
+++ semester III/cobajavadieclipse/src/cobajavadieclipse/testingzzz.java
@@ -0,0 +1,10 @@
+package cobajavadieclipse;
+
+public class testingzzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("halo manusai cerdas");
+	}
+
+}
