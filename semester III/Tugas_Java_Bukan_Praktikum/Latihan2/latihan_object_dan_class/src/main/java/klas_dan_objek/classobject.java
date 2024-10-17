2e0e3b18fdeb789dcacc3509286c59341c368ca1
AppleCraft <jovangilbert737@gmail.com>
2024-10-17 08:51:55 +0800

    menambahkan menjadi 3 objek

--- semester III/Tugas_Java_Bukan_Praktikum/Latihan2/latihan_object_dan_class/src/main/java/klas_dan_objek/classobject.java
+++ semester III/Tugas_Java_Bukan_Praktikum/Latihan2/latihan_object_dan_class/src/main/java/klas_dan_objek/classobject.java
@@ -18,15 +18,18 @@ public class classobject {
     public static void main(String[] args) {
         KonversiJarak jarakKampus = new KonversiJarak();
         KonversiJarak jarakBandara = new KonversiJarak();
+        KonversiJarak jarakULMBJB = new KonversiJarak();
       //mengisi atribut class 
         jarakKampus.jarakKM = 5;
-        jarakBandara.jarakKM = 28;
+        jarakBandara.jarakKM = 23;
+        jarakULMBJB.jarakKM = 32;
 
       //memanggil isi atribut 
         System.out.println("jarak yang harus ditempuh oleh Rizki dari rumah ke kampus adalah " + 
         jarakKampus.toMeter(jarakKampus.jarakKM) + " meter dan " + jarakKampus.toCentimeter(jarakKampus.jarakKM) + " centimeter");
         System.out.println("jarak yang harus ditempuh oleh Damar dari rumah ke Bandara adalah " + 
         jarakBandara.toMeter(jarakBandara.jarakKM) + " meter dan " + jarakBandara.toCentimeter(jarakBandara.jarakKM) + " centimeter");
-
+        System.out.println("jarak yang harus ditempuh oleh Jovan dari rumah ke ULM Banjarbaru adalah " + 
+        jarakULMBJB.toMeter(jarakULMBJB.jarakKM) + " meter dan " + jarakULMBJB.toCentimeter(jarakULMBJB.jarakKM) + " centimeter");
     }
 }
