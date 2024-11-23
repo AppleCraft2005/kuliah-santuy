f28226e7246228c726118c8ee8510e5976448da3
AppleCraft <jovangilbert737@gmail.com>
2024-11-23 22:31:30 +0800

    memisahkan class soal 2

---
+++ semester III/Praktikum/Modul3_Pemro_II_JovanGilbertNatamasindah_2310817310002/praktikum3_soal2/src/main/java/soal2/Negara.java
@@ -0,0 +1,30 @@
+package soal2;
+
+class Negara {
+    String nama;
+    String jenis_kepemimpinan;
+    String nama_pemimpin;
+    String tanggal_kemerdekaan;
+    String bulan_kemerdekaan;
+    String tahun_kemerdekaan;
+
+    Negara(String Nama, String Jenis_kepemimpinan, String Nama_pemimpin, String Tanggal_kemerdekaan, String Bulan_kemerdekaan, String Tahun_kemerdekaan) {
+        this.nama = Nama;
+        this.jenis_kepemimpinan = Jenis_kepemimpinan;
+        this.nama_pemimpin = Nama_pemimpin;
+        this.tanggal_kemerdekaan = Tanggal_kemerdekaan;
+        this.bulan_kemerdekaan = Bulan_kemerdekaan;
+        this.tahun_kemerdekaan = Tahun_kemerdekaan;
+    }
+
+    void showDetail() {
+        if(jenis_kepemimpinan.equals("monarki")) {
+            System.out.println("Negara " + nama + " mempunyai Raja bernama " + nama_pemimpin );
+        }
+        else {
+            System.out.println("Negara " + nama + " mempunyai " + jenis_kepemimpinan + " bernama " + nama_pemimpin + " Deklarasi Kemerdekaan pada Tanggal " + tanggal_kemerdekaan + " " + bulan_kemerdekaan + " " + tahun_kemerdekaan);
+
+        }
+    }
+
+}
