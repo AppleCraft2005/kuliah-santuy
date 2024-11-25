8509340e7e3980c6392dfb310bf39b810e560fac
AppleCraft <jovangilbert737@gmail.com>
2024-11-25 19:10:48 +0800

    menyelesaikan nomor 3

---
+++ semester III/Praktikum/Modul3_Pemro_II_JovanGilbertNatamasindah_2310817310002/praktikum3_soal3/src/main/java/soal3/Mahasiswa.java
@@ -0,0 +1,19 @@
+package soal3;
+
+class Mahasiswa {
+    String nama;
+    String nim;
+    
+    Mahasiswa(String Nama, String NIM) {
+        this.nama = Nama;
+        this.nim = NIM;
+    }
+
+    String getNama() {
+        return nama;
+    }
+
+    String getNim() {
+        return nim;
+    }
+}
