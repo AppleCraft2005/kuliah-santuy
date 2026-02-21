fe0c4bc8c5ff38fc659f846f001a2f9faae68032
AppleCraft2005 <jovangilbert737@gmail.com>
2026-02-21 16:47:23 +0800

    menghapus beberapa hal

--- semesterIV/Pemrograman-Mobile/MobileUAS/app/src/main/java/com/android/modul5/domain/usecase/CheckFavMovieUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.android.modul5.domain.usecase
-
-import com.android.modul5.domain.repository.MovieRepository
-
-class CheckFavMovieUseCase(private val repository: MovieRepository) {
-    suspend operator fun invoke(movieId: Int): Boolean {
-        return repository.isMovieFav(movieId)
-    }
-}
--- semesterIV/Pemrograman-WEB-iI/Modul6/app/Models/HomeModel.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace app\Models;
-
-class HomeModel {
-    protected $data = ['nama' => 'Jovan Gilbert Natamasindah', 'nim' => '2310817310002'];
-
-    public function getPraktikan() {return $this -> data;}
-}
-?>
